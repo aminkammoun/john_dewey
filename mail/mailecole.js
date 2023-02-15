a8436a1bb0478666c231c61c764317afceefcf8e
aminkammoun <aminekammoun55@gmail.com>
2023-02-15 19:23:04 +0100

    email

--- mail/mailecole.js
+++ mail/mailecole.js
@@ -14,8 +14,8 @@ function sendMail() {
     //message: document.getElementById("message").value,
   };
 
-  const serviceID = "service_m02gfj6";
-  const templateID = "template_ee1tlfy";
+  const serviceID = "service_5dyyf6p";
+  const templateID = "template_wvqy2wa";
 
   emailjs.send(serviceID, templateID, params).then(
     function (response) {
